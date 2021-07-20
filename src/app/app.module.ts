fed75ba09e4c4bc3540ba03d8f01258f8f0d1152
Julien <gercevalcode@gmail.com>
2021-07-20 15:18:04 +0200

    structure, header radiant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardComponent } from './components/card/card.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonOutlineComponent } from './components/button-outline/button-outline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    HeaderComponent,
+    ButtonOutlineComponent
   ],
   imports: [
     BrowserModule

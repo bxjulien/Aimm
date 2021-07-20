94cac3c168402ea3e61ffe0098c127595f87a356
Julien <julienberthoumieux.dev@gmail.com>
2021-07-20 18:34:54 +0200

    desktop

--- src/app/components/button-outline/button-outline.component.ts
+++ src/app/components/button-outline/button-outline.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button-outline',
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonOutlineComponent implements OnInit {
 
-
-  text = "bonjour"
+  @Input() text;
+  @Input() iconPath;
+  @Input() iconAlt;
 
   constructor() { }
 
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() iconPath;
+  @Input() iconAlt;
+
+  @Input() title;
+  @Input() description;
+  
   constructor() { }
 
   ngOnInit(): void {
